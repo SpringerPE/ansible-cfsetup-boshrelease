6b95f6ab56d7cfac9ad389d3880d3accf13e378f
Jose Riguera <jriguera@gmail.com>
2017-02-02 19:48:45 +0100

    new role submodule

--- jobs/ansible-cfsetup/templates/ansible/env.sh
+++ jobs/ansible-cfsetup/templates/ansible/env.sh
@@ -6,10 +6,3 @@ else
     ANSIBLE_ROLES_PATH=/var/vcap/packages/ansible-cfsetup/roles:${ANSIBLE_ROLES_PATH}
 fi
 export ANSIBLE_ROLES_PATH
-
-if [ -z ${ANSIBLE_LIBRARY+x} ]; then
-    ANSIBLE_LIBRARY=/var/vcap/packages/ansible-cfsetup/library
-else
-    ANSIBLE_LIBRARY=/var/vcap/packages/ansible-cfsetup/library:${ANSIBLE_LIBRARY}
-fi
-export ANSIBLE_LIBRARY
